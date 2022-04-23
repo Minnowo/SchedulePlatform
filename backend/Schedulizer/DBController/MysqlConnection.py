27c039c7dca0b8da35e0366010f547c175d68f9c
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2022-04-22 22:30:45 -0400

    Added MySQL port number and comments relating to db connection

--- backend/Schedulizer/DBController/MysqlConnection.py
+++ backend/Schedulizer/DBController/MysqlConnection.py
@@ -1,3 +1,9 @@
+"""Initial connection for the MySQL database.
+
+TODO: Warning MySQL username, password and database are currently stored plaintext via secretsVPS.py, which is git
+ ignored. Code should instead be using environment variables at the least for production.
+"""
+
 import mysql.connector
 
 from backend.secretsVPS import SQL_HOST, SQL_USERNAME, SQL_PASSWORD, SQL_DB
@@ -7,10 +13,11 @@ def get_connection():
     db_connection = None
     try:
         db_connection = mysql.connector.connect(
-            host=SQL_HOST,
+            host=SQL_HOST,  # Local host = "localhost" or "127.0.0.1"
             user=SQL_USERNAME,
             passwd=SQL_PASSWORD,
             database=SQL_DB,
+            port=3306,  # MySQL default port number is 3306.
             autocommit=True  # Autocommit 1 = True, 0 = False. To check on status Server $ mysql > select @@autocommit;
         )
     except Exception as err:
