0cc56e262a433f08776afb090fc692902d008208
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2022-04-23 21:36:55 -0400

    Corrected useless constants file path

--- backend/Schedulizer/constants.py
+++ backend/Schedulizer/constants.py
@@ -8,9 +8,6 @@ from backend.Schedulizer.SemesterConfigHandler import decode_config
 # Cache file directory
 CACHE_DIRECTORY_PATH = "cache/"
 
-# Semester config files directory
-CONFIG_DIRECTORY_PATH = "Schedulizer/configs/"
-
 # ICS Calendar base file name
 ICS_CALENDAR_FILENAME = "calendar.ics"
 
@@ -22,4 +19,4 @@ CLASS_INSTRUCTION_IN_PERSON_KEYS = ["in-class", "in-person"]  # API data at .ins
 OUTDATED_COURSE_METADATA_TIMEDELTA = timedelta(days=0, hours=0, minutes=30, seconds=0)
 
 # Enabled semester config templates
-ENABLED_CONFIGS_FILE_PATH = CONFIG_DIRECTORY_PATH + "0_enabledConfigs.json"
+ENABLED_CONFIGS_FILE_PATH = "Schedulizer/configs/0_enabledConfigs.json"
