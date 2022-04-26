2ca6950b1a8aed2c20c1103ed2f321d0ed80f356
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2022-04-26 11:14:10 -0400

    Switched to using env variables instead of secret .py - Updated gitignore and README.md

--- backend/Schedulizer/DBController/MysqlConnection.py
+++ backend/Schedulizer/DBController/MysqlConnection.py
@@ -1,23 +1,22 @@
 """Initial connection for the MySQL database.
-
-TODO: Warning MySQL username, password and database are currently stored plaintext via secretsVPS.py, which is git
- ignored. Code should instead be using environment variables at the least for production.
 """
 
+from dotenv import load_dotenv
+import os
 import mysql.connector
 
-from backend.secretsVPS import SQL_HOST, SQL_USERNAME, SQL_PASSWORD, SQL_DB
+load_dotenv()
 
 
 def get_connection():
     db_connection = None
     try:
         db_connection = mysql.connector.connect(
-            host=SQL_HOST,  # Local host = "localhost" or "127.0.0.1"
-            user=SQL_USERNAME,
-            passwd=SQL_PASSWORD,
-            database=SQL_DB,
-            port=3306,  # MySQL default port number is 3306.
+            host=os.getenv("SQL_HOST"),  # Local host = "localhost" or "127.0.0.1"
+            user=os.getenv("SQL_USERNAME"),
+            passwd=os.getenv("SQL_PASSWORD"),
+            database=os.getenv("SQL_DB"),
+            port=os.getenv("SQL_PORT"),  # MySQL default port number is 3306.
             autocommit=True  # Autocommit 1 = True, 0 = False. To check on status Server $ mysql > select @@autocommit;
         )
     except Exception as err:
