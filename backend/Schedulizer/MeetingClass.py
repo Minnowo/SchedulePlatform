d9400e23f615c1112c57ddc2f5d39ce16c2531ca
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2022-04-29 18:19:50 -0400

    Improved Meeting attribute value defaulting and date shifting logic fix. - Documentation and formatting corrections.

--- backend/Schedulizer/MeetingClass.py
+++ backend/Schedulizer/MeetingClass.py
@@ -28,8 +28,9 @@ class Meeting(BaseModel):
                 location='UOW SYN SYN')
     """
 
-    time_start: time
-    time_end: time
+    time_start: time = time.min
+    time_end: time = time.max
+    # In the event times are not specified, assume all day Meeting.
     weekday_int: int
     date_start: date
     date_end: date
@@ -42,44 +43,52 @@ class Meeting(BaseModel):
         Returns:
             First datetime.date of the actual date which the meeting starts.
 
-        Say a Meeting object has a self.weekday_int of 0 (representing a meeting on Monday), but has a
-        self.date_start value that has a weekday (int) value that is not 0.
-            Under certain circumstances a Meeting object can have a self.date_start that does not fall on the
-            self.weekday_int. This commonly occurs with Courses running repeating meetings weekly/biweekly, due to
-            the way schools might set their date start and end. They often set start and end dates of weekly
-            courses with the start and end of the semester.
-        This function works to return the correct first date of the in which the Meeting should be on.
+        Say a Meeting object has a self.weekday_int of 0 (representing a
+        meeting on Monday), but has a self.date_start value that has a weekday
+        (int) value that is not 0.
+            Under certain circumstances a Meeting object can have a
+            self.date_start that does not fall on the self.weekday_int. This
+            commonly occurs with Courses running repeating meetings
+            weekly/biweekly, due to the way schools might set their date start
+            and end. They often set start and end dates of weekly courses with
+            the start and end of the semester.
         """
 
         # Note I created/utilized this nested function since this is a really useful function that might one day be
         # needed outside this class structure.
 
-        def get_minimum_on_after_date_of_target_weekday(target_weekday_int: int, base_date: date) -> date:
-            """Get the first instance of a date that matches the target weekday in that falls on or after the base date.
+        def forward_weekday_target(target_weekday_int: int, base_date: date) \
+                -> date:
+            """Get the first instance of a date that matches the target weekday
+            in that falls on or after the base date.
 
             Args:
                 target_weekday_int: Target weekday we want on the date.
-                    Follows datetime.datetime.weekday() index convention. (0 = Monday, 1 = Tuesday, ..., 6 = Sunday)
+                    Follows datetime.datetime.weekday() index convention.
+                    (0 = Monday, 1 = Tuesday, ..., 6 = Sunday)
                 base_date: Initial date to start on.
 
             Returns:
                 New date with the correct target weekday.
 
             Examples:
-                >>> get_minimum_on_after_date_of_target_weekday(target_weekday_int=0,base_date=date(2022, 4, 1))
+                >>> forward_weekday_target(target_weekday_int=0,base_date=date(2022, 4, 1))
                 datetime.date(2022, 4, 4)
-                >>> get_minimum_on_after_date_of_target_weekday(target_weekday_int=4,base_date=date(2022, 4, 1))
+                >>> forward_weekday_target(target_weekday_int=4,base_date=date(2022, 4, 1))
                 datetime.date(2022, 4, 1)
-                >>> get_minimum_on_after_date_of_target_weekday(target_weekday_int=5,base_date=date(2022, 4, 1))
+                >>> forward_weekday_target(target_weekday_int=5,base_date=date(2022, 4, 1))
                 datetime.date(2022, 4, 2)
             """
-            target_delta_int = target_weekday_int - base_date.weekday()  # Calculate the shift required
-            target_delta_int += 7 if target_delta_int < 0 else 0  # If your target is Monday and the start_time =
-            # Wednesday, target_delta_int shifts to the next future Monday (Not going backwards to a past Monday)
+            target_delta_int = target_weekday_int - base_date.weekday()
+            # Calculate the shift required
+            target_delta_int += 7 if target_delta_int < 0 else 0  # If your
+            # target is Monday and the start_time = Wednesday, target_delta_int
+            # shifts to the next future Monday (Not going backwards to a past
+            # Monday).
 
             return base_date + timedelta(days=target_delta_int)  # Shifted date
 
-        return get_minimum_on_after_date_of_target_weekday(self.weekday_int, self.date_start)
+        return forward_weekday_target(self.weekday_int, self.date_start)
 
     def get_actual_date_end(self) -> date:
         """Get the actual date of the last matching weekday_int.
@@ -87,38 +96,42 @@ class Meeting(BaseModel):
         Returns:
             Last datetime.date of the actual date which the meeting ends.
 
-        This function need and behaviour is very similar to that of self.get_actual_date_start().
+        This function need and behaviour is very similar to that of
+        self.get_actual_date_start().
         """
 
-        # Note I created/utilized this nested function since this is a really useful function that might one day be
-        # needed outside this class structure.
-
-        def get_minimum_on_before_date_of_target_weekday(target_weekday_int: int, base_date: date) -> date:
-            """Get the last instance of a date that matches the target weekday in that falls on or before the base date.
+        def backward_target_weekday(target_weekday_int: int, base_date: date) \
+                -> date:
+            """Get the last instance of a date that matches the target weekday
+            in that falls on or before the base date.
 
             Args:
                 target_weekday_int: Target weekday we want on the date.
-                    Follows datetime.datetime.weekday() index convention. (0 = Monday, 1 = Tuesday, ..., 6 = Sunday)
+                    Follows datetime.datetime.weekday() index convention.
+                    (0 = Monday, 1 = Tuesday, ..., 6 = Sunday)
                 base_date: Initial date to start on.
 
             Returns:
                 New date with the correct target weekday.
 
             Examples:
-                >>> get_minimum_on_before_date_of_target_weekday(target_weekday_int=0,base_date=date(2022, 4, 30))
+                >>> backward_target_weekday(target_weekday_int=0,base_date=date(2022, 4, 30))
                 datetime.date(2022, 4, 25)
-                >>> get_minimum_on_before_date_of_target_weekday(target_weekday_int=4,base_date=date(2022, 4, 30))
+                >>> backward_target_weekday(target_weekday_int=4,base_date=date(2022, 4, 30))
                 datetime.date(2022, 4, 29)
-                >>> get_minimum_on_before_date_of_target_weekday(target_weekday_int=5,base_date=date(2022, 4, 30))
+                >>> backward_target_weekday(target_weekday_int=5,base_date=date(2022, 4, 30))
                 datetime.date(2022, 4, 30)
             """
-            target_delta_int = target_weekday_int - base_date.weekday()  # Calculate the shift required
-            target_delta_int -= 7 if target_delta_int > 0 else 0  # If your target is Monday and the start_time =
-            # Wednesday, target_delta_int shifts to the previous past Monday (Not going forward to the future Monday).
+            target_delta_int = target_weekday_int - base_date.weekday()
+            # Calculate the shift required
+            target_delta_int -= 7 if target_delta_int > 0 else 0  # If your
+            # target is Monday and the start_time = Wednesday, target_delta_int
+            # shifts to the previous past Monday (Not going forward to the
+            # future Monday).
 
             return base_date + timedelta(days=target_delta_int)  # Shifted date
 
-        return get_minimum_on_before_date_of_target_weekday(self.weekday_int, self.date_start)
+        return backward_target_weekday(self.weekday_int, self.date_end)
 
     def get_raw_str(self):
         return (f"time_start={self.time_start}\n"
